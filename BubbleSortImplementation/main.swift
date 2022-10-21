7353101e8eaff4eea86fb157cae219de892a88e9
lcs-ashin <ashin@lcs.on.ca>
2022-10-21 11:12:01 -0400

    Create a variable to count the number of comparisons occurred each pass. (Reset the number after each pass.) If no more comparison occurs, exit the program.

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -17,6 +17,9 @@ func waitForUserInput() {
 // Create an empty list (array)
 var dataSet: [Int] = []
 
+// Create a variable that counts the number of swaps in a pass
+var numberOfSwaps = 0
+
 // Populate the list
 for _ in 1...10 {
     dataSet.append(Int.random(in: 1...100))
@@ -31,6 +34,8 @@ waitForUserInput()
 // (however many times there are elements in the array)
 for i in 0..<dataSet.count {
     
+    numberOfSwaps = 0
+    
     // One pass through the array to float the highest number to the end
     for j in 0..<dataSet.count - 1 {
         
@@ -44,15 +49,24 @@ for i in 0..<dataSet.count {
             dataSet[j + 1] = temporaryValue // Replace right with the temporary value
             print(" values were swapped.", terminator: "")
             
+            numberOfSwaps = numberOfSwaps + 1
+            
         }
         print("")
         
     }
-
+    
     // Print the array after the pass
     print("Array after pass \(i + 1):")
     print(dataSet)
-    waitForUserInput()
+    
+    // End the program if no more comparison occurs
+    if numberOfSwaps == 0 {
+        exit(0)
+    } else {
+        waitForUserInput()
+    }
+   
 
 }
 
